28a8e885add2041c3923a8ef29cc74d9aebc0bf0
jhereth <jhereth@quda.de>
2019-06-09 11:38:45 +0200

    fingerprint working

---
+++ README.md
@@ -0,0 +1,9 @@
+# barch
+
+Tool to support backup and archival
+
+## Development Notes
+
+### poetry
+
+This project uses [poetry](https://github.com/sdispater/poetry) for dependency management and packaging.
---
+++ tests/test_fingerprint.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from unittest import TestCase
+
+import barch.fingerprint
+import barch.fingerprint
+
+TESTING = (Path(__file__).parent / '..' / 'testing').resolve()
+
+
+# current_directory = os.path.split(path_to_current_file)[0]
+# path_to_file = os.path.join(current_directory, "mydata.json")
+# with open(path_to_file) as mydata:
+#     my_json_data = json.load(mydata)
+
+class TestFingerprint(TestCase):
+    def test_fingerprint(self):
+        assert (
+                barch.fingerprint.fingerprint(
+                    TESTING / 'fingerprint.txt'
+                ) == '20af7fdfb0586595'
+        )
